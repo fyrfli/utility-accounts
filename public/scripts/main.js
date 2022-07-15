4dea70974305e5f8796da9fef2d505461dd52f3a
Camille Frantz <camille.frantz@me.com>
2022-07-15 11:21:04 -0500

    gonna need to start over

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,5 +6,23 @@ jsyear.innerText = todayDate.getFullYear();
 
 const resultFld = document.querySelector(".results");
 const optionsSelect = document.querySelectorAll(".lnk");
+const dataFile = "assets/accounts.json";
+const dataArr = [];
+let i = 0;
 
-resultFld.innerText = Object.keys(optionsSelect);
+fetch(dataFile)
+.then((response) => {
+  if (!response.ok) {
+    resultFld.innerText = "Fetch returned: " + response;
+  }
+  else {
+    return response.json();
+  }
+})
+.then((data) => {
+  for(i in data) {
+    dataArr.push([i], data [i]);
+  }
+  resultFld.innerText = dataArr[0];
+})
+
